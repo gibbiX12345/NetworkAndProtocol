1c6d61ee0a7a975c4f2879c9b1e2e8fffad4d6f2
Julian Luginbühl <julian.s.luginbuehl@iet-gibb.ch>
2016-11-01 08:35:47 +0100

    Getters and Setters vergessen

--- src/protocol/clientToServer/DropBomb.java
+++ src/protocol/clientToServer/DropBomb.java
@@ -10,7 +10,21 @@ public class DropBomb extends SuperPlayerMessage {
     private Integer positionX;
     private Integer positionY;
 
+    public Integer getPositionX() {
+        return positionX;
+    }
 
+    public void setPositionX(Integer positionX) {
+        this.positionX = positionX;
+    }
+
+    public Integer getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(Integer positionY) {
+        this.positionY = positionY;
+    }
 
     public DropBomb(String playerName, Integer positionX, Integer positionY) {
         super(playerName);
