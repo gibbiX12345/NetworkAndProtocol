5d09f44ba3228bb616cff024af85f541c4c1184c
Julian Luginbühl <julian.s.luginbuehl@iet-gibb.ch>
2016-11-08 09:35:16 +0100

    Broadcast und Send Methoden gemäss Klassendiagramm

--- src/network/Server.java
+++ src/network/Server.java
@@ -1,12 +1,20 @@
 package network;
 
 
+import com.sun.javafx.collections.MappingChange;
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.rmi.server.ExportException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by vmadmin on 01.11.2016.
@@ -14,9 +22,13 @@ import java.net.Socket;
 
 public class Server extends Thread {
     private Socket client;
+    private static List<Socket> clientList = new ArrayList<>();
+    private static HashMap<String, Socket> clientConntectionMap = new HashMap<>();
 
     public Server(Socket client) {
         this.client = client;
+        Server.clientList.add(client);
+        Server.clientConntectionMap.put(client.getRemoteSocketAddress().toString(), client);
     }
 
     public void run() {
@@ -26,24 +38,68 @@ public class Server extends Thread {
             out.println("Hallo, ich bin der Server");
 
             String input;
-            while ((input = in.readLine()) != null){
-                out.println(input);
+            while (true){
+                input = in.readLine();
+                System.out.println("Client(" + client.getRemoteSocketAddress() + "): " + input);
+                //out.println(input);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Client(" + client.getRemoteSocketAddress() + ")disconnected");
         } finally {
             try {
                 if (client != null) {
                     client.close();
                 }
             } catch (IOException e){
-
+                e.printStackTrace();
             }
+        }
+    }
+
+    public static void broadcast(String message){
+        for (Socket client : clientList){
+            try {
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                out.println(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void send(String message, String connectionId){
+        Socket client = Server.clientConntectionMap.get(connectionId);
+        try {
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+            out.println(message);
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
 
     public static void main (String[] args){
         int port = Integer.parseInt(args[0]);
+
+        Thread readLineThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String line;
+                BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+                try {
+                    while ((line = input.readLine()) != null) {
+                        if (line.length() == 0)
+                            break;
+
+                        Server.broadcast(line);
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        readLineThread.start();
 
         try (ServerSocket server = new ServerSocket(port) ){
             System.out.println("Server auf " + port + " gestartet ...");
---
+++ src/network/ServerProxy.java
@@ -0,0 +1,59 @@
+package network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by vmadmin on 01.11.2016.
+ */
+public class ServerProxy {
+    private static PrintWriter writer;
+
+    public static void main(String[] args){
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        try{
+            Socket socket = new Socket(host, port);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println(in.readLine());
+
+            System.out.println("> ");
+            String line;
+            Thread serverThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Socket socket = new Socket(host, port);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                        while (true) {
+                            String input = in.readLine();
+                            System.out.println("Server: " + input);
+                        }
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+            serverThread.start();
+            while ((line = input.readLine()) != null){
+                if (line.length() == 0)
+                    break;
+
+                send(line);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void send (String message){
+        writer.println(message);
+    }
+}
